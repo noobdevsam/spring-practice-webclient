77eb8e734822052b28e74f0062841be23be8de14
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-18 08:05:47 +0600

    initialize Spring Boot web client project with Maven wrapper and basic configuration

---
+++ src/main/java/com/example/springpracticewebclient/SpringPracticeWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticewebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeWebclientApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeWebclientApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/springpracticewebclient/SpringPracticeWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticewebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeWebclientApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
