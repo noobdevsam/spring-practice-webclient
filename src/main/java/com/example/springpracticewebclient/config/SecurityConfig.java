df9be85e6fd6b902b0d72ef040c0fba2092aa3dc
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-25 20:14:25 +0600

    add SecurityConfig for OAuth2 authorized client management

---
+++ src/main/java/com/example/springpracticewebclient/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.springpracticewebclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+		ReactiveClientRegistrationRepository clientRegistrationRepository,
+		ReactiveOAuth2AuthorizedClientService authorizedClientService
+	) {
+		var authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+			                               .clientCredentials()
+			                               .build();
+		
+		var authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+			clientRegistrationRepository, authorizedClientService
+		);
+		
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+		return authorizedClientManager;
+	}
+}
