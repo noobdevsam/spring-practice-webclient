2ea708393d0c17b784818e91b6d270303e4338c5
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-25 23:21:07 +0600

    add OAuth2 support to WebClient configuration

--- src/main/java/com/example/springpracticewebclient/config/WebClientConfig.java
+++ src/main/java/com/example/springpracticewebclient/config/WebClientConfig.java
@@ -3,19 +3,30 @@ package com.example.springpracticewebclient.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
 	
 	private final String baseUrl;
+	private final ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
 	
-	public WebClientConfig(@Value("${webclient.baseUrl}") String baseUrl) {
+	public WebClientConfig(
+		@Value("${webclient.baseUrl}") String baseUrl,
+		ReactiveOAuth2AuthorizedClientManager authorizedClientManager
+	) {
 		this.baseUrl = baseUrl;
+		this.authorizedClientManager = authorizedClientManager;
 	}
 	
 	@Override
 	public void customize(WebClient.Builder webClientBuilder) {
-		webClientBuilder.baseUrl(baseUrl);
+		
+		var oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+		oauth.setDefaultClientRegistrationId("springauth");
+		
+		webClientBuilder.filter(oauth).baseUrl(baseUrl);
 	}
 }
