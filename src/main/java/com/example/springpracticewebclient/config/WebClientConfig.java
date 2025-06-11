c63815f1ff8591cdeac1041e2a1ca1f0b730bbed
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-11 09:54:36 +0600

    add Logbook filter to WebClient configuration for enhanced logging

--- src/main/java/com/example/springpracticewebclient/config/WebClientConfig.java
+++ src/main/java/com/example/springpracticewebclient/config/WebClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
@@ -26,7 +28,14 @@ public class WebClientConfig implements WebClientCustomizer {
 		
 		var oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
 		oauth.setDefaultClientRegistrationId("springauth");
-		
-		webClientBuilder.filter(oauth).baseUrl(baseUrl);
+
+		var logbookWebFilter = new LogbookExchangeFilterFunction(
+				Logbook.builder().build()
+		);
+
+		webClientBuilder
+				.filter(oauth)
+				.filter(logbookWebFilter)
+				.baseUrl(baseUrl);
 	}
 }
